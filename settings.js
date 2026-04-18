6c5850d0ad6144b3ed6c30fb201cad282c2ca82c
evandiriski888-svg <evandiriski888@gmail.com>
2026-04-18 13:21:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ export default {
 
   // If you don't have session id, use pairing code
   // Put your WhatsApp number here (without +)
-  PAIRING_NUMBER: "",
+  PAIRING_NUMBER: "6287836689814",
   
   SESSION_SITE: "https://pair.malvintech.sbs",
 
